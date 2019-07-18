960c56be63412ae5d46c096843e0c86cd4ecb63c
QAPaul20vd <paulbvd@gmail.com>
2019-07-18 21:00:04 +0300

    Initial commit

---
+++ src/test/java/org/fasttrackit/LoginTest.java
@@ -0,0 +1,33 @@
+package org.fasttrackit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    @Test
+    public void valinLoginTest() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        accountButton.click();
+
+        WebElement loginLink = driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a"));
+        loginLink.click();
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("firstkidtest@mailinator.com");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("");
+        driver.findElement(By.cssSelector("#send2")).click();
+
+        driver.quit();
+
+
+
+
+
+    }
+}
