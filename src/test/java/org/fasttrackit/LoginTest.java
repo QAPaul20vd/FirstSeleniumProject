0a79b0f33e21075ecc1203008962cc961eab7db1
QAPaul20vd <paulbvd@gmail.com>
2019-07-24 21:55:24 +0300

    Tests

--- src/test/java/org/fasttrackit/LoginTest.java
+++ src/test/java/org/fasttrackit/LoginTest.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class LoginTest {
 
@@ -67,6 +68,24 @@ public class LoginTest {
     public void emptyFieldsTest() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        accountButton.click();
+
+        WebElement loginLink = driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a"));
+        loginLink.click();
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("");
+        driver.findElement(By.cssSelector("#send2")).click();
+
+//        driver.quit();
+    }
+
+    @Test
+    public void emptyFieldsTestMozilla() {
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
         WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
         accountButton.click();
