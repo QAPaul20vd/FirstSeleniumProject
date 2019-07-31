30f818521f24bc1ea98d7ab6d0f4d25a8658ffca
QAPaul20vd <paulbvd@gmail.com>
2019-07-31 11:18:51 +0300

    JUnit&CSS

--- src/test/java/org/fasttrackit/RegisterTest.java
+++ src/test/java/org/fasttrackit/RegisterTest.java
@@ -1,12 +1,15 @@
 package org.fasttrackit;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class RegisterTest {
     private static WebDriver driver;
@@ -24,19 +27,25 @@ public class RegisterTest {
 
         driver.get("https://fasttrackit.org/selenium-test/");
 
-        WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        WebElement accountButton = driver.findElement(By.cssSelector(".skip-account"));
         accountButton.click();
 
-        WebElement registerButton = driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a"));
+        WebElement registerButton = driver.findElement(By.cssSelector("a[title^=Reg]"));
         registerButton.click();
 
-        driver.findElement(By.id("firstname")).sendKeys("Auto");
+//        Fill in all the required fields
+        driver.findElement(By.id("firstname")).sendKeys("Auto3");
         driver.findElement(By.id("middlename")).sendKeys("Ini");
         driver.findElement(By.id("lastname")).sendKeys("Test");
-        driver.findElement(By.name("email")).sendKeys("QAautotest@mailinator.com");
+        driver.findElement(By.name("email")).sendKeys("QAautotest3@mailinator.com");
         driver.findElement(By.name("password")).sendKeys("autotest123");
         driver.findElement(By.id("confirmation")).sendKeys("autotest123");
-        driver.findElement(By.xpath("//*[@id=\"is_subscribed\"]")).click();
+
+//        Click on Register button
+        driver.findElement(By.cssSelector("button[title=Register]")).click();
+
+//        Verify that the user account was created
+        Assert.assertTrue(driver.findElement(By.cssSelector(".success-msg")).isDisplayed());
 
     }
 
@@ -55,8 +64,13 @@ public class RegisterTest {
         driver.findElement(By.id("lastname")).sendKeys("Test");
         driver.findElement(By.name("email")).sendKeys("QAautotest@mailinator.com");
         driver.findElement(By.name("password")).sendKeys("autotest123");
+//        Put an invalid password
         driver.findElement(By.id("confirmation")).sendKeys("autotest");
         driver.findElement(By.xpath("//*[@id=\"is_subscribed\"]")).click();
+
+        driver.findElement(By.cssSelector("button[title=Register]")).click();
+//        Verify that an error message is displayed
+        Assert.assertTrue(driver.findElement(By.cssSelector(".validation-advice")).isDisplayed());
 
     }
 
@@ -74,10 +88,17 @@ public class RegisterTest {
         driver.findElement(By.id("firstname")).sendKeys("Auto");
         driver.findElement(By.id("middlename")).sendKeys("Ini");
         driver.findElement(By.id("lastname")).sendKeys("Test");
+
+//        Complete with an invalid email
         driver.findElement(By.name("email")).sendKeys("QAautotest@mailinator");
         driver.findElement(By.name("password")).sendKeys("autotest123");
         driver.findElement(By.id("confirmation")).sendKeys("autotest");
         driver.findElement(By.xpath("//*[@id=\"is_subscribed\"]")).click();
+
+        driver.findElement(By.cssSelector("button[title=Register]")).click();
+
+//        Verify that an error message is displayed
+        Assert.assertTrue(driver.findElement(By.cssSelector(".validation-advice")).isDisplayed());
     }
 
     @AfterClass
--- src/test/java/org/fasttrackit/WishListTest.java
+++ src/test/java/org/fasttrackit/WishListTest.java
@@ -1,6 +1,7 @@
 package org.fasttrackit;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -19,26 +20,76 @@ public class WishListTest {
         driver.manage().window().maximize();
     }
 
-
     @Test
-    public void addToWishList() {
+    public void addToWishListUserNotLoggedIn() {
 
         driver.get("https://fasttrackit.org/selenium-test/");
 
+//        Navigate to Sale Menu
         WebElement saleButton = driver.findElement(By.cssSelector(".nav-5"));
         saleButton.click();
 
-        WebElement detailsButton = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout >" +
-                "div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(1) > div > div.actions > a"));
+//        Navigate to Detail page of the first product
+        WebElement detailsButton = driver.findElement(By.cssSelector(".products-grid > li:first-child a[title^=View]"));
         detailsButton.click();
 
+//        Verify that the user is not Logged In
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("a[title='Log In']")).isDisplayed());
+
+//        Add product to wishlist
         WebElement addToWishlist = driver.findElement(By.cssSelector(".link-wishlist"));
         addToWishlist.click();
 
+//        Verify that the user is redirected to the Login Page
+        WebElement loginButton = driver.findElement(By.cssSelector("button[title='Login']"));
+        Assert.assertTrue(loginButton.isDisplayed());
     }
 
-//    @AfterClass
-//    public static void quit() {
-//        driver.quit();
-//    }
+    @Test
+    public void addToWishListUserLoggedIn(){
+
+        driver.get("https://fasttrackit.org/selenium-test/");
+
+//        Login with valid account
+        WebElement accountButton = driver.findElement(By.cssSelector(".skip-account"));
+        accountButton.click();
+
+        WebElement loginLink = driver.findElement(By.cssSelector("a[title^=Log]"));
+        loginLink.click();
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("QAautotest@mailinator.com");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("autotest123");
+        driver.findElement(By.cssSelector("#send2")).click();
+
+//        Verify that the User is Logged In
+        WebElement helloText = driver.findElement(By.cssSelector(".welcome-msg .hello"));
+        Assert.assertEquals("Hello, Auto Test!", helloText.getText());
+
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("a[title='Log Out']")).isDisplayed());
+
+//        Navigate to Sale Menu
+        WebElement saleButton = driver.findElement(By.cssSelector(".nav-5"));
+        saleButton.click();
+
+//      Navigate to Detail page of the first product
+        WebElement detailsButton = driver.findElement(By.cssSelector(".products-grid > li:first-child a[title^=View]"));
+        detailsButton.click();
+
+//        Add product to WishList
+        WebElement addToWishList = driver.findElement(By.cssSelector(".link-wishlist"));
+        addToWishList.click();
+
+//        Verify that the product is added to My WishList
+        WebElement myWishList = driver.findElement(By.cssSelector(".my-wishlist"));
+        Assert.assertTrue(myWishList.isDisplayed());
+    }
+
+
+
+    @AfterClass
+    public static void quit() {
+        driver.quit();
+    }
 }
